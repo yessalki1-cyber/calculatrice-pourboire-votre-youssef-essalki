872e663d556b743793a2498ef26ae1574b55a3ab
Youssef Essalki <yessalki1@gmail.com>
2026-07-09 10:55:00 +0100

    feat :ajout du calcul js

---
+++ script.js
@@ -0,0 +1,15 @@
+const montant = document.getElementById("montant");
+const resultat = document.getElementById("resultat");
+
+montant.addEventListener("input", function () {
+
+    let note = parseFloat(montant.value);
+
+    if (!isNaN(note)) {
+        let pourboire = note * 0.15;
+        resultat.textContent = "Pourboire : " + pourboire.toFixed(2) + " DH";
+    } else {
+        resultat.textContent = "";
+    }
+
+});
