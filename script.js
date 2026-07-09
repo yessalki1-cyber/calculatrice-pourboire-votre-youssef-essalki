5f5f20c575cd74313f98c7126417cc32b9756444
Youssef Essalki <yessalki1@gmail.com>
2026-07-09 11:31:30 +0100

    modifier html

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ montant.addEventListener("input", function () {
     let note = parseFloat(montant.value);
 
     if (!isNaN(note)) {
-        let pourboire = note * 0.15;
+        let pourboire = note * 0.20;
         resultat.textContent = "Pourboire : " + pourboire.toFixed(2) + " DH";
     } else {
         resultat.textContent = "";
